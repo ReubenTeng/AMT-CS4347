c39f0b5a9255ae7964789a910f5ab63a29ec4a9e
Cheok Su Anne <e0424761@u.nus.edu>
2022-11-04 11:57:21 +0800

    testing demucs

---
+++ source_separation/demucs.py
@@ -0,0 +1,8 @@
+# series of commands to run demucs:
+# conda env update -f environment-cpu.yml
+# conda activate demucs
+# pip install -e .
+# demucs PATH_TO_AUDIO_FILE_1 [PATH_TO_AUDIO_FILE_2 ...]
+
+
+# import subprocess
